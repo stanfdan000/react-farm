fa8f51b92f4eb333d9a72bf45cb81152464f7769
sirdan <stanford303@gmail.com>
2022-06-07 14:20:34 -0700

    added folders and tried to get back ground img to render

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.css';
+import backgroundImg from '../background.png';
+
+export default function Main() {
+  return (
+    <main style={{ backGroundImage: `url(${backgroundImg})` }}>
+
+    </main>
+  );
+}
